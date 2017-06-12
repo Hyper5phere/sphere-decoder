18a886575bfc1e47d6e5affcc0a5aa40d6a35c86
Pasi Pyrrö <pasi.pyrro@aalto.fi>
2017-06-12 11:19:02 +0300

    inital commit

---
+++ sphdec-slurm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -t 00:00:15
+#SBATCH --mem-per-cpu=2500
+#SBATCH -p debug
+
+echo "Starting sphere decoder simulation..."
+echo "Loading module Armadillo/7.800.2-iomkl-triton-2017a-Python-2.7.13"
+module restore arma-env
+
+g++ sphdec.cpp -o sphdec -O2 -larmadillo -llapack -lblas
+
+srun sphdec
