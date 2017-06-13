e20bb5697123db70dd65e456750a21475deb1cf7
Pasi Pyrrö <pasi.pyrro@aalto.fi>
2017-06-13 15:07:43 +0300

    Matrix (Mathematica format) parser implemented

--- src/sphdec.cpp
+++ src/sphdec.cpp
@@ -2,6 +2,8 @@
  * Compile with: g++ sphdec.cpp -o sphdec -O2 -larmadillo -llapack -lblas -std=c++14
  */
 
+// #define ARMA_NO_DEBUG // for speed
+
 #include <iostream>
 #include <armadillo>
 #include <complex>
@@ -12,9 +14,12 @@
 #include <map>
 #include <algorithm>
 #include <regex>
+#include <locale>
 
 using namespace std;
 using namespace arma;
+
+string default_filename = "options.ini";
 
 void clean_input(string &input){
     // clean out comments
@@ -23,6 +28,7 @@ void clean_input(string &input){
         input = input.substr(0, start);
     // clean out white spaces
     input.erase(remove_if(input.begin(), input.end(), ::isspace), input.end());
+    // input.erase(remove(input.begin(), input.end(), '+'), input.end());
 }
 
 void create_config(string filepath){
@@ -32,10 +38,10 @@ void create_config(string filepath){
             << "word_file=words.txt         // Text file containing the words" << endl \
             << "output_file=output.txt      // Text file used for simulation output" << endl \
             << "code_size=4                 // Size of the code" << endl \
-            << "code_length=8               // Length of the code" << endl \
-            << "no_of_matrices=4            // Number of basis matrices" << endl \
-            << "no_of_broadcast_antennas=5  // Number of broadcast antennas" << endl \
-            << "no_of_receiver_antennas=5   // Number of receiver antennas"  << endl \
+            << "code_length=2               // Length of the code" << endl \
+            << "no_of_matrices=1            // Number of basis matrices" << endl \
+            << "no_of_broadcast_antennas=2  // Number of broadcast antennas" << endl \
+            << "no_of_receiver_antennas=2   // Number of receiver antennas"  << endl \
             << "snr_min=6                   // Minimum value for signal-to-noise ratio" << endl \
             << "snr_max=12                  // Maximum value for signal-to-noise ratio" << endl \
             << "initial_radius=-1           // Initial search radius for the sphere decoder" << endl \
@@ -51,8 +57,8 @@ map<string, string> get_options(string filepath){
 
     if (!config_file.good()){
         cout << "Warning: no config file found, creating a default one..." << endl;
-        create_config(filepath);
-        config_file = ifstream(filepath);
+        create_config(default_filename);
+        config_file = ifstream(default_filename);
     }
 
     string line;
@@ -76,28 +82,84 @@ map<string, string> get_options(string filepath){
     return output;
 }
 
-vector<cx_mat> read_matrices(string filepath){
-    //regex elem_regex("(\\{|,)\\s*(\\+|\\-)?(\\d*).(\\d+)(\\+|\\-)?\\s*(\\d*).(\\d+)\\s[Ii]\\s*(,|\\})\\s*");
-    regex elem_regex("(\\d+).\\d+");
-    ifstream matrix_file(filepath);
+vector<cx_mat> read_matrices(map<string, string> &options){
+
+    regex elem_regex(
+        "(([+-]?\\s*\\d*\\.?\\d+|[+-]?\\s*\\d+\\.?\\d*[Ii]?)|"
+        "([+-]?\\s*\\d*\\.?\\d+|[+-]?\\s*\\d+\\.?\\d*)" // parse any float
+        "\\s*"
+        "([+-]?\\s*\\d*\\.?\\d+|[+-]?\\s*\\d+\\.?\\d*)" // parse any float
+        "[Ii]{1})\\s*([,}]{1})"  
+    );
+
+    uint m = atoi(options.at("no_of_receiver_antennas").c_str());
+    uint n = atoi(options.at("code_length").c_str());
+    uint k = atoi(options.at("no_of_matrices").c_str());
+
+    ifstream matrix_file(options.at("basis_file"));
     vector<cx_mat> output;
-    string line;
-    smatch m;
-    while (getline(matrix_file, line))
-    {
-        // cout << line << endl;
-        if (regex_search(line, m, elem_regex)) {
-            for(auto const &match : m)
-                cout << match.str() << endl;
+    smatch match;
+    vector< complex<double> > numbers;
+    
+    string content((istreambuf_iterator<char>(matrix_file)), 
+        istreambuf_iterator<char>());
+    
+    uint idx = 0;
+    while(regex_search(content, match, elem_regex)){
+        // cout << content << endl;
+        if (!match.empty()) {
+            // matches.push_back(match);
+            // for(auto i = 0u; i < match.size(); ++i)
+            //     cout << i << ": " << match[i].str() << endl;
+            // cout << m[0].str() << endl;
+            string z = match[1].str();
+            if (count(z.begin(), z.end(), '.') > 1){
+                string a = match[3].str(), b = match[4].str();
+                clean_input(a); clean_input(b);
+                // cout << stod(a) << "+" << stod(b) << "i" << endl;
+                numbers.push_back(complex<double>(stod(a),stod(b)));
+            } else {
+                clean_input(z);
+                if (isdigit(z[z.length()-1])) {
+                    // cout << stod(z) << endl;
+                    numbers.push_back(complex<double>(stod(z), 0.0)); 
+                } else {
+                    // cout << stod(z) << "i" << endl;
+                    numbers.push_back(complex<double>(0.0, stod(z)));
+                }
+            }
+        } else {
+            return output;
         }
+        content = match.suffix().str();
+        idx++;
     }
-    output.push_back(cx_mat(5,5,arma::fill::randu));
+
+    if (m*n*k != numbers.size()){
+        cout << "Failed to read configured " <<  m*n*k << " matrix elements!" << endl;
+        exit(1);
+    }
+
+    idx = 0;
+    cx_mat X(m,n);
+    for (auto i = 0u; i < k; i++){    
+        // cout << i << X << endl;
+        for (auto j = 0u; j < m; j++){
+            for (auto s = 0u; s < n; s++){
+                X(j,s) = numbers[idx];
+                idx++;
+            }
+        }
+        output.push_back(X);
+        X = cx_mat(m, n);
+    }
+    
     return output;
 }
 
 int main(int argc, char** argv)
 {
-    string inputfile = "options.ini";
+    string inputfile = default_filename;
     if (argc > 1){
         inputfile = argv[1];
     }
@@ -106,9 +168,9 @@ int main(int argc, char** argv)
     //     cout << item.first << " = " << item.second << endl;
     // }
 
-    auto bases = read_matrices(output["basis_file"]);
-    // for (auto const &base : bases){
-    //     cout << base << endl;
-    // }
+    auto bases = read_matrices(output);
+    for (auto const &base : bases){
+        cout << base << endl;
+    }
     return 0;
 }
