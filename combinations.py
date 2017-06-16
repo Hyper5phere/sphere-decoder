b3c769334ae34189b1be50aefbba95b5aa7067f7
Pasi Pyrrö <pasi.pyrro@aalto.fi>
2017-06-16 14:23:16 +0300

    working pseudocode for calculating all possible code vectors implemted

--- combinations.py
+++ combinations.py
@@ -1,14 +1,28 @@
 # test script for the combinations algorithm
 # (does not work)
 
-def combinations(symbols, combination, dim):
-    curr = []
-    if dim == -1:
-        return [combination]
-    else:
-        for s in symbols:
-            combination[dim] = s
-            curr += combinations(symbols, combination, dim-1)
-        return curr
+comblist = []
 
-print(repr(combinations([1,2,3], [1,1,1], 2)))
+def comb_wrapper(symbset, dim):
+    a = dim*[symbset[0]]
+    return combinations(symbset, a, dim-1)
+
+def combinations(symbols, combination, dim):
+    if combination not in comblist:
+        comblist.append(combination)
+
+    if dim >= 0:
+        for s in symbols:
+            tmp = combination.copy()
+            tmp[dim] = s
+            combinations(symbols, tmp, dim-1)
+
+symbset = [3, -1, 1, 3]
+code_length = 4
+
+comb_wrapper(symbset, code_length)
+print("All possible combinations for code vector:")
+for i, c in enumerate(comblist):
+    print(str(c) + "\t", end="")
+    if (i+1) % code_length == 0:
+        print("")
